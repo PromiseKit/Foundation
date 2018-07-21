bb0814308f347e4ba0141556dfbd3406c21fac6c
Doug <dougstein@gmail.com>
2018-07-20 17:21:57 -0700

    'Cancel' for PromiseKit -- temporarily require Swift 4.0 for SPM so that the .branch parameter can be used; temporarily point to the forked PromiseKit and branch PMKCancel

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
+// swift-tools-version:4.0
+// temporarily require swift 4.0 so the .branch specifier below will work, remove these 2 lines before integrating
 import PackageDescription
 
 let package = Package(
     name: "PMKFoundation",
     dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
+//      .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6)
+        .Package(url: "https://github.com/dougzilla32/PromiseKit.git", .branch("PMKCancel")),
 //      .Package(url: "https://github.com/PromiseKit/Cancel.git", majorVersion: 1),
         .Package(url: "https://github.com/dougzilla32/Cancel.git", majorVersion: 1)
     ],
