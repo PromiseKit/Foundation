3098825db7471538250576a8b8068b140be19062
Doug <dougstein@gmail.com>
2018-07-24 17:02:08 -0700

    'Cancel' for PromiseKit -- fix SPM build by not requiring SPM 4 as we are building with both SPM 3 and 4
    'Cancel' for PromiseKit -- remove accidental 'print' statement in the tests

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
-// swift-tools-version:4.0
-// temporarily require swift 4.0 so the .branch specifier below will work, remove these 2 lines before integrating
 import PackageDescription
 
 let package = Package(
     name: "PMKFoundation",
     dependencies: [
-//      .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6)
-        .Package(url: "https://github.com/dougzilla32/PromiseKit.git", .branch("PMKCancel")),
+        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
+// Change this to point to 'PromiseKit/Cancel' before integrating
 //      .Package(url: "https://github.com/PromiseKit/Cancel.git", majorVersion: 1),
         .Package(url: "https://github.com/dougzilla32/Cancel.git", majorVersion: 1)
     ],
