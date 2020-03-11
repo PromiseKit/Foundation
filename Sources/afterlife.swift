ed9fa1f5141f5dc68b7617fceb9a5b679ea527e2
Luka Bratos <luka.bratos@gmail.com>
2020-03-11 17:56:39 +0000

    Fix typo

--- Sources/afterlife.swift
+++ Sources/afterlife.swift
@@ -5,7 +5,7 @@ import PromiseKit
 
 /**
  - Returns: A promise that resolves when the provided object deallocates
- - Important: The promise is not guarenteed to resolve immediately when the provided object is deallocated. So you cannot write code that depends on exact timing.
+ - Important: The promise is not guaranteed to resolve immediately when the provided object is deallocated. So you cannot write code that depends on exact timing.
  */
 public func after(life object: NSObject) -> Guarantee<Void> {
     var reaper = objc_getAssociatedObject(object, &handle) as? GrimReaper
